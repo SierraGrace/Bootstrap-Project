f429400983265fb9ef6e21ff97c7f7ff05692cf7
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-13 00:55:17 +0300

    Added log in system

--- php/signin.php
+++ php/signin.php
@@ -1,1 +1,14 @@
-<?php?>
+<?php
+	$login = $_POST['loginInput'];
+	$password = $_POST['passwordInput'];
+
+	$mysql = new mysqli('localhost', 'root', '', 'users-db');
+	$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+	$mysql->close();
+
+	if($result->num_rows > 0) {
+		echo "Welcome";
+	} else {
+		echo "Wrong login or password";
+	}
+?>
