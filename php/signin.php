27fdd63825b870695f24e7a68a6c9026ee363924
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-13 01:08:06 +0300

    Added checking for empty fields in signup/in.php

--- php/signin.php
+++ php/signin.php
@@ -2,13 +2,17 @@
 	$login = $_POST['loginInput'];
 	$password = $_POST['passwordInput'];
 
-	$mysql = new mysqli('localhost', 'root', '', 'users-db');
-	$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
-	$mysql->close();
-
-	if($result->num_rows > 0) {
-		echo "Welcome";
+	if(empty($login) || empty($password)) {
+		echo "Empty fields";
 	} else {
-		echo "Wrong login or password";
+		$mysql = new mysqli('localhost', 'root', '', 'users-db');
+		$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+		$mysql->close();
+
+		if($result->num_rows > 0) {
+			echo "Welcome";
+		} else {
+			echo "Wrong login or password";
+		}
 	}
 ?>
--- php/signup.php
+++ php/signup.php
@@ -9,17 +9,17 @@
 		$isAdmin = 0;
 	}
 
-	if(mb_strlen($uname) > 50) {
+	if(empty($uname) || mb_strlen($uname) > 50) {
 		echo "User name error";
-	} else if(mb_strlen($login) > 50) {
+	} else if(empty($login) || mb_strlen($login) > 50) {
 		echo "Login error";
-	} else if(mb_strlen($password) > 32) {
+	} else if(empty($password) || mb_strlen($password) > 32) {
 		echo "Password error";
+	} else {
+		$mysql = new mysqli('localhost', 'root', '', 'users-db');
+		$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$uname', '$login', '$password', '$isAdmin')");
+		$mysql->close();
+
+		header('Location: http://localhost/Bootstrap-Project');
 	}
-
-	$mysql = new mysqli('localhost', 'root', '', 'users-db');
-	$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$uname', '$login', '$password', '$isAdmin')");
-	$mysql->close();
-
-	header('Location: http://localhost/Bootstrap-Project');
 ?>
