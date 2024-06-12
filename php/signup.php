42b1a1b8b0ade4cb0987f6224cddf8a3df768224
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-12 21:57:26 +0300

    Added uploading of sign up data to the database

--- php/signup.php
+++ php/signup.php
@@ -1,1 +1,23 @@
-<?php?>
+<?php
+	$uname = $_POST['unameInput'];
+	$login = $_POST['loginInput'];
+	$password = $_POST['passwordInput'];
+
+	if(isset($_POST['isAdmin'])) {
+		$isAdmin = 1;
+	} else {
+		$isAdmin = 0;
+	}
+
+	if(mb_strlen($uname) > 50) {
+		echo "User name error";
+	} else if(mb_strlen($login) > 50) {
+		echo "Login error";
+	} else if(mb_strlen($password) > 32) {
+		echo "Password error";
+	}
+
+	$mysql = new mysqli('localhost', 'root', '', 'users-db');
+	$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$uname', '$login', '$password', '$isAdmin')");
+	$mysql->close();
+?>
