20a8b95bc6901a646917d00696367caac6b7f479
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-13 10:22:41 +0300

    Added returning to sign in form in case of invalid data

--- php/signin.php
+++ php/signin.php
@@ -3,7 +3,7 @@
 	$password = $_POST['passwordInput'];
 
 	if(empty($login) || empty($password)) {
-		echo "Empty fields";
+		echo "Empty fields<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign in form</a>";
 	} else {
 		$mysql = new mysqli('localhost', 'root', '', 'users-db');
 		$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
@@ -12,7 +12,7 @@
 		if($result->num_rows > 0) {
 			echo "Welcome";
 		} else {
-			echo "Wrong login or password";
+			echo "Wrong login or password<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign in form</a>";
 		}
 	}
 ?>
