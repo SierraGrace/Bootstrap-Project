21c65fe12b24f46b4bc8138922bf99d03eed3381
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-14 12:20:12 +0300

    Edited html code
    Edited html code according to HTML Code Style Conventions

--- pages/admin_page.php
+++ pages/admin_page.php
@@ -1,36 +1,36 @@
 <!DOCTYPE html>
 <?php
-	session_start();
+  session_start();
 
-	$mysql = new mysqli('localhost', 'root', '', 'users-db');
-	$data = $mysql->query("SELECT * FROM `users`");
-	$mysql->close();
+  $mysql = new mysqli('localhost', 'root', '', 'users-db');
+  $data = $mysql->query("SELECT * FROM `users`");
+  $mysql->close();
 ?>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="../css/bootstrap.min.css">
-		<title>Admin page</title>
-	</head>
-	<body>
-		<form id="logoutForm" action="../php/logout.php" method="post">
-			<h4>Welcome, Administrator <?=$_SESSION['userName']?>!</h4>
-			<?php
-				while ($dataResult = $data->fetch_assoc()) {
-			?>
-				<div class="row">
-					<div class="col">Id: <?=$dataResult['id']?></div>
-					<div class="col">User name: <?=$dataResult['user_name']?></div>
-					<div class="col">Login: <?=$dataResult['login']?></div>
-					<div class="col">Password: <?=$dataResult['password']?></div>
-					<div class="col">Is admin: <?=$dataResult['is_admin']?></div>
-				</div>
-		    <?php
-				}
-			?>
-			<button type="submit" class="btn btn-danger">Log out</button>			
-		</form>
-		<script src="../js/bootstrap.bundle.min.js"></script>
-	</body>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>Admin page</title>
+  </head>
+  <body>
+    <form id="logoutForm" action="../php/logout.php" method="post">
+      <h4>Welcome, Administrator <?=$_SESSION['userName']?>!</h4>
+      <?php
+        while ($dataResult = $data->fetch_assoc()) {
+      ?>
+          <div class="row">
+            <div class="col">Id: <?=$dataResult['id']?></div>
+            <div class="col">User name: <?=$dataResult['user_name']?></div>
+            <div class="col">Login: <?=$dataResult['login']?></div>
+            <div class="col">Password: <?=$dataResult['password']?></div>
+            <div class="col">Is admin: <?=$dataResult['is_admin']?></div>
+          </div>
+      <?php
+        }
+      ?>
+      <button type="submit" class="btn btn-danger">Log out</button>			
+    </form>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
--- pages/user_page.php
+++ pages/user_page.php
@@ -1,19 +1,19 @@
 <!DOCTYPE html>
 <?php
-	session_start();
+  session_start();
 ?>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" href="../css/bootstrap.min.css">
-		<title>User page</title>
-	</head>
-	<body>
-		<form id="logoutForm" action="../php/logout.php" method="post">
-			<h4>Welcome, <?=$_SESSION['userName']?>!</h4>
-			<button type="submit" class="btn btn-danger">Log out</button>			
-		</form>
-		<script src="../js/bootstrap.bundle.min.js"></script>
-	</body>
+<html lang="en-us">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <title>User page</title>
+  </head>
+  <body>
+    <form id="logoutForm" action="../php/logout.php" method="post">
+      <h4>Welcome, <?=$_SESSION['userName']?>!</h4>
+      <button type="submit" class="btn btn-danger">Log out</button>			
+    </form>
+    <script src="../js/bootstrap.bundle.min.js"></script>
+  </body>
 </html>
