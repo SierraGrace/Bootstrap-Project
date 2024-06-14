56d9924526dee96930b32d0371cae2dca4d697f8
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-14 12:51:07 +0300

    Edited php code
    Edited php code according to PHP Code Style Conventions

--- php/signin.php
+++ php/signin.php
@@ -2,23 +2,23 @@
 	$login = $_POST['loginInput'];
 	$password = $_POST['passwordInput'];
 
-	if(empty($login) || empty($password)) {
+	if (empty($login) || empty($password)) {
 		echo "Empty fields<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign in form</a>";
 	} else {
 		$mysql = new mysqli('localhost', 'root', '', 'users-db');
 		$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
 		
-		if($result->num_rows > 0) {
+		if ($result->num_rows > 0) {
 			$userName = $mysql->query("SELECT `user_name` FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
-			$nameResult = $userName->fetch_assoc();
+			$nameQueryResult = $userName->fetch_assoc();
 
 			session_start();
-			$_SESSION['userName'] = $nameResult['user_name'];
+			$_SESSION['userName'] = $nameQueryResult['user_name'];
 
-			$isAdmin = $mysql->query("SELECT `is_admin` FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
-			$isAdminResult = $isAdmin->fetch_assoc();
+			$isAdminQuery = $mysql->query("SELECT `is_admin` FROM `users` WHERE `login` = '$login' AND `password` = '$password'");
+			$isAdminQueryResult = $isAdminQuery->fetch_assoc();
 
-			if($isAdminResult['is_admin'] == 0) {
+			if ((int)$isAdminQueryResult['is_admin'] === 0) {
 				$mysql->close();
 				header('Location: ../pages/user_page.php');
 			} else {
--- php/signup.php
+++ php/signup.php
@@ -1,32 +1,32 @@
 <?php
-	$uname = $_POST['unameInput'];
+	$userName = $_POST['unameInput'];
 	$login = $_POST['loginInput'];
 	$password = $_POST['passwordInput'];
 
-	if(isset($_POST['isAdmin'])) {
+	if (isset($_POST['isAdmin'])) {
 		$isAdmin = 1;
 	} else {
 		$isAdmin = 0;
 	}
 
-	if(empty($uname) || mb_strlen($uname) > 50) {
+	if (empty($userName) || mb_strlen($userName) > 50) {
 		echo "User name error<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign up form</a>";
-	} else if(empty($login) || mb_strlen($login) > 50) {
+	} else if (empty($login) || mb_strlen($login) > 50) {
 		echo "Login error<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign up form</a>";
-	} else if(empty($password) || mb_strlen($password) > 32) {
+	} else if (empty($password) || mb_strlen($password) > 32) {
 		echo "Password error<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign up form</a>";
 	} else {
 		$mysql = new mysqli('localhost', 'root', '', 'users-db');
-		$result = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login'");
+		$queryResult = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login'");
 
-		if($result->num_rows > 0) {
-			echo "Login has already been used by another user<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign up form</a>";
-			$mysql->close();
-		} else {
-			$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$uname', '$login', '$password', '$isAdmin')");
+		if ((int)$queryResult->num_rows === 0) {
+			$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$userName', '$login', '$password', '$isAdmin')");
 			$mysql->close();
 
 			header('Location: http://localhost/Bootstrap-Project');
+		} else {
+			echo "Login has already been used by another user<br><a href=\"http://localhost/Bootstrap-Project\">Back to sign up form</a>";
+			$mysql->close();
 		}
 	}
 ?>
