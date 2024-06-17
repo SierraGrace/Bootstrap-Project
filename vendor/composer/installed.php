79d4ea2e3055e780e085ec630c2a69f755260d23
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-17 12:52:14 +0300

    Added Workerman framework

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '5dc9a4854748c575ff2e2d8f0b818031c5d4631f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '5dc9a4854748c575ff2e2d8f0b818031c5d4631f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'workerman/workerman' => array(
+            'pretty_version' => 'v4.1.15',
+            'version' => '4.1.15.0',
+            'reference' => 'afc8242fc769ab7cf22eb4ac22b97cb59d465e4e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../workerman/workerman',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
