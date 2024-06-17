44cd3fef2d90026572eaa3d6602d84bb69f7b217
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-17 13:02:21 +0300

    Added event handlers to socket server

--- php/socket_server.php
+++ php/socket_server.php
@@ -4,4 +4,20 @@
 
 	$worker = new Worker('websocket://localhost:8001');
 	$worker->count = 4;
+
+	$worker->onConnect = function ($connection) {
+		echo "New connection\n";
+	};
+
+	$worker->onMessage = function ($connection, $data) use ($worker) {
+		foreach($worker->connections as $clientConnection) {
+			$clientConnection->send($data);
+		}
+	};
+
+	$worker->onClose = function ($connection) {
+		echo "Connection closed\n";
+	};
+
+	Worker::runAll();
 ?>
