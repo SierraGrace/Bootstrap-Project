bba3590d275bd8c33a33dc4519181f9b5c943198
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-18 14:27:13 +0300

    Moved socket server file

--- php/socket_server.php
+++ php/socket_server.php
@@ -1,6 +1,6 @@
 <?php
 	use Workerman\Worker;
-	require_once __DIR__ . '/vendor/autoload.php';
+	require_once __DIR__ . '/../vendor/autoload.php';
 
 	$worker = new Worker('websocket://localhost:8001');
 	$worker->count = 4;
--- socket_server_start.bat
+++ socket_server_start.bat
@@ -1,1 +1,1 @@
-cmd /k php C:\xampp\htdocs\Bootstrap-Project\socket_server.php
+cmd /k php C:\xampp\htdocs\Bootstrap-Project\php\socket_server.php
