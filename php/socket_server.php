80aea05d387c48550cd7ab5cc096f244ce1a4407
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-19 17:47:44 +0300

    Added debug to server

--- php/socket_server.php
+++ php/socket_server.php
@@ -8,12 +8,22 @@
 
 	$worker->onConnect = function ($connection) {
 		$connections[$connection->id] = $connection;
+
 		echo "New connection\n";
+		echo "ID: " . $connection->id . "\n";
+		echo "IP: " . $connection->getRemoteIp() . "\n";
+   		echo "Remote port: " . $connection->getRemotePort() . "\n";
+   		echo "Used prototocol: " . $connection->protocol . "\n";
+   		//print_r($connection);
 	};
-	
+
 	$worker->onMessage = function ($connection, $data) use ($worker) {
+		// post form login is-admin >>>  add conn id to array
+		// 
 		foreach($worker->connections as $clientConnection) {
+			// if cliconn->id == admin conn id >>> send mess
 			$clientConnection->send($data);
+			echo $data;
 		}
 	};
 
