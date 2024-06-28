7b897ebc6ddb1bcbae68231e64c4dc2d401a15c5
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-28 19:33:17 +0300

    Added simple output test to sign_up.php

--- project-reimagining/php/sign_up.php
+++ project-reimagining/php/sign_up.php
@@ -1,1 +1,11 @@
-<?php?>
+<?php
+	$username = $_POST['username'];
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+    $admin = isset($_POST['adminCheck']) ? 1 : 0;
+
+    echo "Username: $username<br>";
+    echo "Login: $login<br>";
+    echo "Password: $password<br>";
+    echo "Admin: $admin<br>";
+?>
