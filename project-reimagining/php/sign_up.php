54b6ebcb61568057d40e823e694f3715fdb7ba71
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-06-28 19:51:02 +0300

    Added data validation for sign_up.php

--- project-reimagining/php/sign_up.php
+++ project-reimagining/php/sign_up.php
@@ -4,12 +4,28 @@
     $password = $_POST['password'];
     $admin = isset($_POST['adminCheck']) ? 1 : 0;
 
-    echo "Username: $username<br>";
-    echo "Login: $login<br>";
-    echo "Password: $password<br>";
-    echo "Admin: $admin<br>";
+    if (empty($username) || mb_strlen($username) > 50) {
+		echo "User name error<br><a href=\"http://localhost/Bootstrap-Project/project-reimagining\">Back to sign up form</a>";
+	} else if (empty($login) || mb_strlen($login) > 50) {
+		echo "Login error<br><a href=\"http://localhost/Bootstrap-Project/project-reimagining\">Back to sign up form</a>";
+	} else if (empty($password) || mb_strlen($password) > 32) {
+		echo "Password error<br><a href=\"http://localhost/Bootstrap-Project/project-reimagining\">Back to sign up form</a>";
+	} else {
 
-    $mysql = new mysqli('localhost', 'root', '', 'users-db');
-    $mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$username', '$login', '$password', '$admin')");
-	$mysql->close();
+	    $mysql = new mysqli('localhost', 'root', '', 'users-db');
+
+	    $queryResult = $mysql->query("SELECT * FROM `users` WHERE `login` = '$login'");
+
+		if ((int)$queryResult->num_rows === 0) {
+			$mysql->query("INSERT INTO `users` (`user_name`, `login`, `password`, `is_admin`) VALUES ('$username', '$login', '$password', '$admin')");
+
+			echo "Registration was successful<br><a href=\"http://localhost/Bootstrap-Project/project-reimagining\">Back to sign up form</a>";
+
+			$mysql->close();
+		} else {
+			echo "Login has already been used by another user<br><a href=\"http://localhost/Bootstrap-Project/project-reimagining\">Back to sign up form</a>";
+
+			$mysql->close();
+		}
+	}
 ?>
