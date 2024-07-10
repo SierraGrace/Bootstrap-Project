f3b7e060c41b3a9a795d3214bf4eaed676075221
Sierra Grace <132104920+SierraGrace@users.noreply.github.com>
2024-07-10 17:12:58 +0300

    Added socket server launch

--- php/socket_server.php
+++ php/socket_server.php
@@ -1,1 +1,11 @@
-<?php?>
+<?php
+	use Workerman\Worker;
+
+	require_once __DIR__ . '/../vendor/autoload.php';
+
+	$worker = new Worker('websocket://localhost:8001');
+
+	echo("Socket server has been started\n");
+
+	Worker::runAll();
+?>
